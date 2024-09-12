54c55247bce055e58eb901b452b92ad66a45a697
KavishBaingane <104kbaingane@gmail.com>
2024-09-12 15:59:12 +0530

    master:Confirgure the webroot path and additional folders.

--- StaticFiles/Program.cs
+++ StaticFiles/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(new
 WebApplicationOptions(){
     WebRootPath = "myroot"
 });
 var app = builder.Build();
 app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath + "\\mywebroot")
+        ) //"workes with mywebroot"
+});
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
